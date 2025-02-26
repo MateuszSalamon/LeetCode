a18d6058f90e7c4c4a555dfcd8ea26ec4fae690f
Mateusz <34424986+MateuszSalamon@users.noreply.github.com>
2025-02-26 08:47:19 +0100

    Create 1749MaximumAbsoluteSumofAnySubarray.c

---
+++ 1749MaximumAbsoluteSumofAnySubarray.c
@@ -0,0 +1,26 @@
+int maxAbsoluteSum(int* nums, int numsSize) {
+
+    int lhs = 0;
+    int rhs = numsSize - 1;
+    int big_sum = 0;
+    int big_diff = 0;
+    int sum_in_window = 0;
+    int index = 0;
+
+    for(index = lsh; i <= rhs; i++)
+    {
+
+        sum_in_window += nums[index];
+        if(sum_in_window > big_sum)
+        {
+            big_sum = sum_in_window;
+        }
+        if(sum_in_window < big_diff)
+        {
+            big_diff = sum_in_window;
+        }
+    }
+
+    return big_sum - big_diff < 0 ? big_diff - big_sum : big_sum - big_diff;
+    
+}
