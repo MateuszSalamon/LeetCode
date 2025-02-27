8d780d608870f38008027f7b88a71722e57533d3
Mateusz <34424986+MateuszSalamon@users.noreply.github.com>
2025-02-27 18:20:46 +0100

    Update 873_LengthofLongestFibonacciSubsequence.c

--- 873_LengthofLongestFibonacciSubsequence.c
+++ 873_LengthofLongestFibonacciSubsequence.c
@@ -1,6 +1,7 @@
 int lenLongestFibSubseq(int* arr, int arrSize) {
     int maxLen = 0;
-    dparr[arrSize][arrSize] = {0};
+    int dparr[arrSize][arrSize];
+    memset(dparr, 0, sizeof(dparr));
 
     for(int curr = 2; curr < arrSize; curr++)
     {
@@ -8,21 +9,21 @@ int lenLongestFibSubseq(int* arr, int arrSize) {
         int end = curr - 1;
         while (start<end)
         {
-            int pair = arr[start] + ar[end];
+            int pair = arr[start] + arr[end];
             if(pair > arr[curr])
             {
                 end--;
             }
-            else if(pair < arr[start])
+            else if(pair < arr[curr])
             {
                 start++;
             }
             else
             {
-                dparr[end][curr] = dp[start][end] + 1;
+                dparr[end][curr] = dparr[start][end] + 1;
                 if(dparr[end][curr] > maxLen)
                 {
-                    maxLen = dp[end][curr];
+                    maxLen = dparr[end][curr];
                 }
                 end--;
                 start++;
