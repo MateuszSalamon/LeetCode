91924865e47540195a84f75957e374def90e81aa
Mateusz <34424986+MateuszSalamon@users.noreply.github.com>
2025-03-24 20:51:19 +0100

    Update 3169CountDaysWithoutMeetings.cpp

--- 3169CountDaysWithoutMeetings.cpp
+++ 3169CountDaysWithoutMeetings.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
         int freedays = 0;
-        qsort(meetings.begin(), meeting.end());
+        sort(meetings.begin(), meetings.end());
 
         int lhs = meetings.front()[0];
         int rhs = meetings.front()[1];
 
         int ans = lhs - 1;
-        for (int i = 1; i < meeting.size(); i++)
+        for (int i = 1; i < meetings.size(); i++)
         {
             int left = meetings[i][0];
             int right = meetings[i][1];
